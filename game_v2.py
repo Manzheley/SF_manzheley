f9e06eafbacda48a521d5d0eed3c25a9ac31c16b
Manzheley <vladimir/manzheley@gmail.com>
2022-08-16 22:02:25 +0300

    initial commit

---
+++ game_v2.py
@@ -0,0 +1,25 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
+import numpy as np
+
+def random_predict(number:int=1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count=0
+    
+    while True:
+        count+=1
+        predict_number = np.random.randint(1, 101) # Предполагаемое число
+        if number == predict_number:
+            break # Выход из цикла если угадали
+    return (count) 
+    
+print(f'Количество попыток: {random_predict(18)}')
